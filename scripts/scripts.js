9f7627dc75feae544b67be2b6b89115e592cc32c
Eric Coutinho <ericoutinho@gmail.com>
2020-06-30 12:48:09 -0300

    add section caparao + js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,4 +6,13 @@ $(window).on('scroll', function(){
         $('.navbar').removeClass('scrolled')
         $('.navbar').addClass('iddle');
     }
-})
+
+    $('.animate').each(function(){
+        var offset = $(window).height() * 3/4;
+        if ($(document).scrollTop() >= ($(this).offset().top - offset)) {
+            if (!$(this).hasClass('reveal')) {
+                $(this).addClass('reveal');
+            }
+        }
+    })
+});
